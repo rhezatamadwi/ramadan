fa6c695468047f41ad8d6e293a115bef730b60aa
Rhezatama Dwi <rhezatama.dwi@gmail.com>
2024-03-17 08:50:26 +0700

    [FIX] layout/welcome: fix css hover

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 <a
                     href="{{ route('kegiatan.show', ['kegiatan' => $keg->id]) }}"
                     id="docs-card"
-                    class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-transparent p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-sky-800 transition duration-300 hover:text-black/70 hover:ring-sky-100 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-2 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
+                    class="flex flex-col group items-start gap-6 overflow-hidden rounded-lg bg-transparent p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-sky-800 transition duration-300 hover:text-black/70 hover:ring-sky-100 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-2 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                 >
 
                     <div class="relative flex items-center gap-6 lg:items-end">
