b05ce485b2246e26c7d3a8b537de2c25e24e66d0
Rhezatama Dwi <rhezatama.dwi@gmail.com>
2024-03-17 07:52:36 +0700

    [FEATURE] add favicon, change title

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Ramadan LPDP {{ now()->year }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="{{ url('/images/favicon-16x16.png') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
