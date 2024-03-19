7eb5b9076152f1ef75df7878b61a4cf02e0cab3e
Rhezatama Dwi <rhezatama.dwi@gmail.com>
2024-03-19 16:46:00 +0700

    [FEATURE] view/dummy: init

---
+++ resources/views/dummy/1.blade.php
@@ -0,0 +1,59 @@
+
+<p class="mt-10 text-justify">
+    <span class="text-lg font-bold">🕌☪ Lantunan Ayat Pembuka Rahmat 
+    Gema Ramadhan : Tingkatkan Iman, Raih Ampunan</span>
+    <br><br>
+    Assalamualaikum #InsanLPDP
+    <br><br>
+    Mari semarakkan Bulan Ramadhan 1445H dengan Lantunan Ayat Suci Al-Qur'an dalam Tilawah Race. Kegiatan Tilawah Ramadhan tahun ini akan berbeda dari tahun sebelumnya karena tilawah dilakukan secara individu dan bagi insan LPDP yang paling banyak mengkhatamkan Al-Qur'an akan mendapatkan hadiah Umroh(*). Berikut ketentuan kegiatan Tilawah Race:
+</p>
+    <ol class="mt-7 text-justify list-decimal px-4">
+        <li>Seluruh Insan LPDP dipersilakan mengikuti kegiatan lomba tilawah ini, seluruh pegawai, driver, OB mendapatkan kesempatan yang sama.</li>
+        <li>Periode pelaksanaan dimulai sejak tanggal 18 Maret 2024 (pkl. 18.30 WIB) sampai dengan 5 April 2024 (pkl. 18.30 WIB) dan Tilawah dimulai dari Q.S Al-Fatihah.</li>
+        <li>Laporan tilawah dapat disetor pada tautan berikut <a class="text-sky-800 hover:text-sky-500" target="_blank" href="https://ramadan.rhezatama.com">https://ramadan.rhezatama.com</a></li>
+        <li>Tata cara laporan tilawah harian dapat dilihat pada link tutorial terlampir
+            <ul class="list-disc  px-4">
+                <li>Cara Register Akun: <a class="text-sky-800 hover:text-sky-500" target="_blank" href="https://youtu.be/43Nokz8ig1M">https://youtu.be/43Nokz8ig1M</a></li>
+                <li>Cara Lapor Tilawah: <a class="text-sky-800 hover:text-sky-500" target="_blank" href="https://youtu.be/2RwU4vUG94M">https://youtu.be/2RwU4vUG94M</a></li> 
+                <li>(Laporkan bacaan terakhir, dimulai dari Q.S. Alfatihah)</li>
+            </ul>
+        </li>
+        <li>Tilawah dilakukan di luar jam kerja sesuai dengan waktu clock in/clock out. Laporan tilawah ketika jam kerja tidak akan dihitung.
+        <li>Pemenang adalah peserta yang paling banyak capaian tilawahnya, dan akan diumumkan di akhir periode kegiatan.
+    </ol>
+</p>
+
+<p class="mt-10 text-justify">
+<span class="text-lg font-bold">🕌☪FAQ Tilawah Race☪🕌</span>
+<br><br>
+Assalamualaikum Insan LPDP sekalian.
+Saking semangatnya tilawah race, banyak yang tanya nih soal ketentuan Tilawah Race supaya lebih jelas mimin rangkumin FAQ Tilawah Race berikut;
+<br><br>
+<ul class="list-disc px-4">
+<li><span class="font-bold">Q: Siapa aja yang boleh ikutan?</span>
+<br>A : Seluruh Insan LPDP ; pegawai, driver, OB boleh mengikuti Tilawah race dan mendapatkan kesempatan yang sama mengejar hadiah utama
+</li>
+
+<li><span class="font-bold">Q: kapan pelaporan Tilawah Race dimulai?</span>
+<br>A : Tilawah Race dimulai sejak tanggal 18 Maret 2024 (pkl. 18.30 WIB) sampai dengan 5 April 2024 (pkl. 18.30 WIB). Pelaporan Sebelum dan Setelah itu tidak dihitung.
+</li>
+
+<li><span class="font-bold">Q: Apakah bacaannya boleh mulai dari tengah-tengah?</span>
+<br>A : Tidak boleh. Untuk memudahkan penghitungan score, pelaporan Tilawah Race dimulai dari Q.S. Al-fatihah.
+</li>
+
+<li><span class="font-bold">Q: kapan boleh tilawahnya?</span>
+<br>A : Tilawah dilakukan diluar jam kerja sesuai dengan waktu clock in/clock out. Boleh juga tilawah di jam istirahat (12.00-12.30 WIB seuai SE) Laporan tilawah ketika jam kerja tidak akan dihitung.
+</li>
+
+<li><span class="font-bold">Q: Ooh, boleh tilawah di jam istirahat?</span>
+<br>A : Boleh, jam 12.00-12.30 WIB sesuai SE
+</li>
+</p>
+
+---
+<p class="mt-4 text-sm/relaxed italic text-justify">
+Orang yang membaca Al-Quran, sedangkan ia masih terbata-bata lagi berat dalam membacanya, maka ia akan mendapatkan dua pahala.” (H.R. Bukhari dan Muslim)
+
+#Eksplo12amadan
+</p>
