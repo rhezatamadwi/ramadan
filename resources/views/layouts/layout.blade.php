33c6e1e770df641da24fab29c52228ab5f44e28e
Rhezatama Dwi <rhezatama.dwi@gmail.com>
2024-03-19 16:43:10 +0700

    [FIX] layouts/modal_tilawah: add info FAQ

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,7 +12,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <link href="{{ asset('css/output.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/output.css') }}?v=0.0.1" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
         <script src="https://kit.fontawesome.com/4c36a673e2.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
